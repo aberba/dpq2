169829474ce1e194427ddb4664aaa82ac9d59819
Denis Feklushkin <feklushkin.denis@gmail.com>
2021-12-17 16:02:40 +0300

    dynloader.d added

---
+++ src/dpq2/dynloader.d
@@ -0,0 +1,73 @@
+///
+module dpq2.dynloader;
+
+/// Available only for dynamic libpq config
+version(DerelictPQ_Dynamic)
+{
+
+import std.traits;
+import dpq2.connection: Connection;
+
+void dynLoader(DG)(scope DG connectionFactory)
+if(isDelegate!DG && is(ReturnType!DG == Connection))
+{
+}
+
+}
+
+immutable class DynamicLoader
+{
+    import derelict.pq.pq: DerelictPQ;
+    import core.sync.mutex: Mutex;
+    import core.atomic;
+    debug import std.experimental.logger;
+
+    private __gshared static Mutex mutex;
+    private shared static ptrdiff_t instances;
+
+    shared static this()
+    {
+        mutex = new Mutex();
+    }
+
+    /*
+        It is possible to setup DerelictPQ.missingSymbolCallback
+        before. So it is possible to use this library even with
+        previous versions of libpq with some missing symbols.
+    */
+    this()
+    {
+        mutex.lock();
+        scope(exit) mutex.unlock();
+
+        if(instances.atomicFetchAdd(1) == 0)
+        {
+            debug trace("DerelictPQ loading...");
+
+            DerelictPQ.load();
+
+            debug trace("...DerelictPQ loading finished");
+        }
+    }
+
+    ~this()
+    {
+        mutex.lock();
+        scope(exit) mutex.unlock();
+
+        if(instances.atomicFetchSub(1) == 1)
+        {
+            import std.stdio;
+            writeln("Unload PQ");
+            DerelictPQ.unload();
+        }
+    }
+
+    import dpq2.connection: Connection;
+
+    /// Accepts same parameters as Connection ctors in static configuration
+    Connection createConnection(T...)(T args)
+    {
+        return new Connection(this, args);
+    }
+}
